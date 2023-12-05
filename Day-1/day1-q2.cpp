8c2b2243c09e2e1ba17677d5072aa819b63fbea5
mattjax16 <mnbass21@colby.edu>
2023-12-04 21:18:14 -0500

    finally added a .gitignore

--- Day-1/day1-q2.cpp
+++ Day-1/day1-q2.cpp
@@ -17,8 +17,6 @@ zoneight234
 In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
  *
 */
-
-
 #include <iostream>
 #include <fstream>
 #include <printf.h>
