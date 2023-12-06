63909533011910a8732e03e94523eb0430258d36
mattjax16 <mnbass21@colby.edu>
2023-12-06 04:13:42 -0500

    day5 q1 with ruby done

--- Day-4/day4-q2.rs
+++ Day-4/day4-q2.rs
@@ -41,8 +41,8 @@ use std::collections::HashMap;
 struct Scratchcard {
     card_number: u32,
     winning_numbers: Vec<u32>,
-    scratchcard_numbers: Vec<u32>
-    win_count: u32
+    scratchcard_numbers: Vec<u32>,
+    win_count: u32,
     number_of_cards: u32
 }
 
@@ -56,6 +56,83 @@ fn read_file(filename: &str) -> Vec<String> {
     }
     scratchcards
 }
+
+// function to get the number of the scratchcard
+fn get_scratchcard_number(scratchcard_str: &str) -> u32 {
+    let scratchcard_number_str = scratchcard_str.split(":").nth(0).unwrap().split(" ").last().unwrap().trim();
+    let scratchcard_number: u32 = scratchcard_number_str.parse().expect("Error parsing scratchcard number");
+    return scratchcard_number;
+}
+
+// Function to parse the scratchcard data
+fn parse_scratchcard(scratchcard_str: &str) -> Scratchcard {
+    // Get the number of the scratchcard
+    let scratchcard_number = get_scratchcard_number(scratchcard_str);
+    let mut clean_scratchcard = scratchcard_str.split(":").nth(1).unwrap();
+    let mut scratchcard_data = clean_scratchcard.split(" | ");
+    let mut winning_numbers = scratchcard_data.next().unwrap().split(" ");
+    let mut scratchcard_numbers = scratchcard_data.next().unwrap().split(" ");
+    let mut scratchcard = Scratchcard {
+        card_number: scratchcard_number,
+        winning_numbers: Vec::new(),
+        scratchcard_numbers: Vec::new(),
+        win_count: 0,
+        number_of_cards: 1
+    };
+
+    for number in scratchcard_numbers {
+        // check if number is empty space and skip if it is
+        if number == "" {
+            continue;
+        }else{
+            scratchcard.scratchcard_numbers.push(number.parse::<u32>().expect("Error parsing scratchcard numbers"));
+        }
+    }
+    for number in winning_numbers {
+       // check if number is empty space and skip if it is
+        if number == "" {
+            continue;
+        }else{
+            scratchcard.winning_numbers.push(number.parse::<u32>().expect("Error parsing winning numbers"));
+        }
+    }
+    scratchcard
+}
+
+// function to calculate the number of winning numbers
+fn calculate_win_count(scratchcard: &mut Scratchcard) {
+    let mut win_count = 0;
+    for number in &scratchcard.winning_numbers {
+        if scratchcard.scratchcard_numbers.contains(number) {
+            win_count += 1;
+        }
+    }
+    scratchcard.win_count = win_count;
+}
+
+// function to calculate the number of copies of each scratchcard won
+fn calculate_number_of_copies(scratchcard_data: &mut Vec<Scratchcard>) {
+    let len = scratchcard_data.len(); // Get the length of the vector
+
+    for i in 0..len {
+        let win_count = scratchcard_data[i].win_count as usize; // Convert to usize
+
+        // for each copy of the scratchcard
+        for k in 0..scratchcard_data[i].number_of_cards {
+            // Check each win of the current scratchcard
+            for j in 0..win_count {
+                // Calculate the index of the next scratchcard
+                let next_index = i + j + 1; // Now both `i` and `j` are of type `usize`
+
+                // Make sure the index is within the bounds of the vector
+                if next_index < len {
+                    scratchcard_data[next_index].number_of_cards += 1;
+                }
+            }
+        }
+    }
+}
+
 
 
 
@@ -71,9 +148,27 @@ fn main() {
     // Read the file into a vector of strings
     let scratchcards = read_file(&std::env::args().nth(1).unwrap());
 
+    // Parse the scratchcard data into a vector of scratchcard structs
+    let mut scratchcard_data: Vec<Scratchcard> = Vec::new();
+    for scratchcard in scratchcards {
+        scratchcard_data.push(parse_scratchcard(&scratchcard));
+    }
+
+    // Calculate the number of winning numbers for each scratchcard
+    for mut scratchcard in &mut scratchcard_data {
+        calculate_win_count(&mut scratchcard);
+    }
+
+    // Calculate the number of copies of each scratchcard won
+    calculate_number_of_copies(&mut scratchcard_data);
+
+    // Calculate the total number of scratchcards won
+    let mut total_scratchcards = 0;
+    for scratchcard in &scratchcard_data {
+        total_scratchcards += scratchcard.number_of_cards;
+    }
+
+    println!("Total number of scratchcards: {}", total_scratchcards);
+
     return;
 }
-
-// Call the main function
-main();
-
